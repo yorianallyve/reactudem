4fc17b1d3b9a16579b33d79852d984ae153c820e
AzureAD\YoriaNallyveRiosAcev <yoria@evolutionxp.com>
2022-04-27 18:17:44 -0500

    .

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap'
+
 
 const MAXIMUN_COUNT = 10;
 
@@ -18,6 +20,10 @@ export const CounterEffect = () => {
 
         console.log('%cSe llego al valor máximo','color: red; background-color: black;')
 
+        const tl = gsap.timeline();
+
+        tl.to( counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out' })
+          .to( counterElement.current, { y: 0, duration:1, ease: 'bounce.out' })
 
     }, [counter])
 
