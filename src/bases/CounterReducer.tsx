f4fd1f7d93a87ab1dbfe34689a260ca829a5f1bc
AzureAD\YoriaNallyveRiosAcev <yoria@evolutionxp.com>
2022-05-02 11:57:28 -0500

    .

--- src/bases/CounterReducer.tsx
+++ src/bases/CounterReducer.tsx
@@ -1,21 +1,42 @@
 import { useReducer } from "react"
 
-interface  CounterState {
-    counter:number;
-    previus:number;
-    change:number
+interface CounterState {
+    counter: number;
+    previus: number;
+    change: number
 }
 
-const INICIAL_STATE= {
-    counter:0,
-    previus:0,
-    change:0
+const INICIAL_STATE = {
+    counter: 0,
+    previus: 0,
+    change: 0
+}
+
+type counterAction =
+    | { type: 'increaseBy', payload: { value: number; } }
+    | { type: 'reset' }
+
+
+const counterReducer = (state: CounterState, action: counterAction): CounterState => {
+    switch (action.type) {
+        case'reset':
+
+            return {
+                counter: 0,
+                previus: 0,
+                change: 0
+            };
+
+        default:
+            return state;
+    }
+
 }
 
 export const CounterReducerComponent = () => {
-    
-    const [ State, dispatch] = useReducer( reducer, initiaState, int );
-    
+
+    const [State, dispatch] = useReducer(reducer, INICIAL_STATE);
+
     const handleClick = () => {
         setCounter(prev => prev + 1)
     }
